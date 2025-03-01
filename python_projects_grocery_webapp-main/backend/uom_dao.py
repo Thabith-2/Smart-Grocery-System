ff03fdfdee4527e12b9b85be8fad6cfd16377a6f
Thabith-2 <145094553+Thabith-2@users.noreply.github.com>
2025-03-01 18:20:21 +0530

    Update uom_dao.py

--- python_projects_grocery_webapp-main/backend/uom_dao.py
+++ python_projects_grocery_webapp-main/backend/uom_dao.py
@@ -1,20 +1,18 @@
 
+## 5. uom_dao.py
+
+```python
 def get_uoms(connection):
     cursor = connection.cursor()
-    query = ("select * from uom")
+    query = ("SELECT uom_id, uom_name FROM uom")
     cursor.execute(query)
+    
     response = []
     for (uom_id, uom_name) in cursor:
         response.append({
             'uom_id': uom_id,
             'uom_name': uom_name
         })
+    
     return response
-
-
-if __name__ == '__main__':
-    from sql_connection import get_sql_connection
-
-    connection = get_sql_connection()
-    # print(get_all_products(connection))
-    print(get_uoms(connection))
+```
