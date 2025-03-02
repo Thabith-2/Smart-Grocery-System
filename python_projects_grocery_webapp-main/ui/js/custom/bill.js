d3a7b31327e815f84ec2de3aa9961c00516d2821
Thabith-2 <145094553+Thabith-2@users.noreply.github.com>
2025-03-02 07:18:23 +0530

    Update bill.js

--- python_projects_grocery_webapp-main/ui/js/custom/bill.js
+++ python_projects_grocery_webapp-main/ui/js/custom/bill.js
@@ -83,4 +83,20 @@ function displayTotal(total) {
     $('#bill-total').text(formatCurrency(total));
 }
 
-// ... rest of the code remains the same ...
+function showError() {
+    $('.bill-container').html(`
+        <div class="alert alert-danger" role="alert">
+            <h4 class="alert-heading">No Order Data Found</h4>
+            <p>We couldn't find any order data to generate a bill. Please create a new order.</p>
+            <hr>
+            <div class="d-flex justify-content-center">
+                <a href="order.html" class="btn btn-primary me-2">
+                    <i class="fas fa-shopping-cart"></i> Create New Order
+                </a>
+                <a href="index.html" class="btn btn-secondary">
+                    <i class="fas fa-home"></i> Go to Dashboard
+                </a>
+            </div>
+        </div>
+    `);
+}
